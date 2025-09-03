53924c5ae981f4c9baa83b98b61b2db36cc58133
unknown <yrigoinneylit@gmail.com>
2025-09-03 08:52:24 -0500

    updt

---
+++ practica-01.R
@@ -0,0 +1,23 @@
+# conceptos basicos de R
+
+# O =|
+# Y = &
+# No = !
+# asignar alt + -
+
+x <- "Hola"
+y <- "UNTRM"
+
+# R es un lenguaje que trabaja con funciones 
+
+# f(x,y) = 2x + x*2
+
+paste(x,y)
+
+#ayuda:F1| Fn + F1 | ?f()
+
+paste(x,y)
+
+#objetos <- "casa"
+
+a
