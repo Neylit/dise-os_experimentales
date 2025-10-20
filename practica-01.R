4461e42c815afde0a085f74710e82ef26d8ae1e2
unknown <yrigoinneylit@gmail.com>
2025-10-20 06:56:40 -0500

    updt

--- practica-01.R
+++ practica-01.R
@@ -20,4 +20,36 @@ paste(x,y)
 
 #objetos <- "casa"
 
-a
+a <- "casa"
+b1 <- "silla"
+b2 <- "cama"
+a_3 <- ""
+
+# Ejemplo
+
+notas <- c(12, 15, 13, 17)
+notas
+md <- mean(notas)
+nf <- md + 2
+nf
+
+#pipe
+
+library(tidyverse)
+
+
+# cntrl + shif + m => %>%
+
+nf <- c(12, 15, 13, 17) %>% 
+  mean() + 2
+  
+nf
+
+
+
+
+
+
+
+
+
---
+++ practica-03.R
@@ -0,0 +1,17 @@
+
+# -------------------------------------------------------------------------
+# Diseños experimentales  -------------------------------------------------
+
+# Diseño factorial --------------------------------------------------------
+
+library(agricolae)
+
+agricolae::design.ab()
+
+
+# factorial 3 x 2 with 3 blocks
+library(agricolae)
+trt<-c(3,2) # factorial 3 x 2
+
+
+ds$book
